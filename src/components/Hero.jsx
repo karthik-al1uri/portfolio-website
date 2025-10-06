ad21036ac37c45b9bf00f4e8b38368b9e630dce8
Karthik Alluri <karthikalluri2003@gmail.com>
2025-10-06 13:57:00 -0600

    hero.js change

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -17,7 +17,7 @@ const Hero = () => {
                 <div>
                     <h1 className={`${styles.heroHeadText} text-white`}>
                         Hi, I'm{" "}
-                        <span className="text-[#915EFF]">Karthik Alluri</span>
+                        <span className="text-[#915EFF]">Karthik Aluri</span>
                     </h1>
                     <p className={`${styles.heroSubText} mt-2 text-white-100`}>
                         I develop data-driven solutions <br className="sm:block hidden" />
